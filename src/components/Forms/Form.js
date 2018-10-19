e61f3b565422886cbbf4f69e681b5d9f40e23bd8
iCharlieAraiza <eliptikman1234@gmail.com>
2018-10-19 09:51:36 -0500

    Added methods Create, Delete and Clean Input

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -11,13 +11,13 @@ const Form = (props)=>{
         <Aux>
             <form className="col-12">
                 <div className="form-group col-md-6 mx-auto">
-                    <input type="text" placeholder="Nombre completo" className="form-control"></input>
+                    <input type="text" name="title" placeholder="Name" className="form-control" onChange={props.change} value={props.title}></input>
                 </div>
                 <div className="form-group col-md-6 mx-auto">
-                    <textarea type="text" placeholder="Nombre completo" className="form-control" style={style.textarea}></textarea>
+                    <textarea type="text" name="body" placeholder="Text" className="form-control" style={style.textarea} onChange={props.change} value={props.body}></textarea>
                 </div>
                 <div className="form-group col-md-6 mx-auto">
-                    <button className="btn btn-primary col-12">Update</button>
+                    <button className="btn btn-primary col-12" onClick={props.submit}>Update</button>
                 </div>
             </form>
         </Aux>
--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -6,12 +6,48 @@ import Posts from '../Posts/Posts'
 class Layout extends Component{
     
     //El estado del componente
+
     state = {
         posts:[
             {id:1, title: "Titulo del artículo", body:"Lorem Ipsul read larte dwet willer roep tyoi sadf berronga"},
             {id:4, title: "Titulo articlo 2", body:"Poyel retyks ckc Ipsul read larte dwet willer roep tyoi sadf berronga"},
-            ]
+            ],
+        
+        current: {title:'', body:''}    
     }
+
+    //Valores del formulario
+
+    currentPost(event){
+        const current = {...this.state.current};
+        
+        if(event.target.name === "title"){
+            current.title = event.target.value;
+        }else{
+            current.body = event.target.value;
+        }
+
+        this.setState({current:current});
+    }
+    
+    //Update 
+    submit(e){
+        e.preventDefault()
+
+        const state = {...this.state};
+
+        if(state.current.title !== '' && state.current.body!==''){
+
+            const post = {id:this.generateId(),title:state.current.title, body:state.current.body}         
+            state.posts.push(post);
+
+            this.setState({posts:state.posts});
+            this.cleanInput();
+        }
+        
+    }
+    
+
 
     //Eliminar
     delete(index){
@@ -24,6 +60,18 @@ class Layout extends Component{
         });
 
     }
+
+
+    cleanInput(){
+        const current={id:0, title: '', body: ''}
+        this.setState({current:current})
+    }
+
+    generateId(){
+        const posts = [...this.state.posts];
+        return posts[posts.length-1].id + 1;
+    }
+
 
     //Elementos que se van a renderizar
     render(){
@@ -39,11 +87,15 @@ class Layout extends Component{
 
         return (
             <Main>
-                <Form></Form>
+                <Form change={this.currentPost.bind(this)} 
+                      submit={this.submit.bind(this)}
+                      title={this.state.current.title}
+                      body={this.state.current.body}></Form>
                 {posts}
+                {console.log(this.state)}
             </Main>
         )
     }
 }
 
-export default Layout;
+export default Layout
